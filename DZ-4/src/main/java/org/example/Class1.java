7a530427fa0de5bd6931ac513065a2760ace5096
Serhii Lvov <slvov4business@gmail.com>
2023-06-20 23:57:03 +0300

    Added simple Java program according to Home

---
+++ DZ-4/src/main/java/org/example/Class1.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Class1 {
+    static public void main(String[] argv){
+
+        Class2 example = new Class2();
+        System.out.println(example.getString());
+
+    }
+
+}
---
+++ DZ-4/src/main/java/org/example/Class2.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Class2
+{
+    Integer intVal1 =24082023;
+    String getString(){
+        return "---<значення поля>---" + intVal1;
+    }
+}
