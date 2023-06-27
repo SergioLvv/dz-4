710ec607d7f3eb85d5c93e75a779775135e860c5
Serhii Lvov <slvov4business@gmail.com>
2023-06-27 18:10:28 +0300

    Added changes according to remarks

--- DZ-4/src/main/java/org/example/Class1.java
+++ DZ-4/src/main/java/org/example/Class1.java
@@ -4,8 +4,9 @@ public class Class1 {
     static public void main(String[] argv){
 
         Class2 example = new Class2();
-        System.out.println(example.getString());
-
+        int returnedValue = example.getValue1();
+        System.out.println("~~~ <" + returnedValue + "> ~~~");
+        
     }
 
 }
--- DZ-4/src/main/java/org/example/Class2.java
+++ DZ-4/src/main/java/org/example/Class2.java
@@ -2,8 +2,11 @@ package org.example;
 
 public class Class2
 {
-    Integer intVal1 =24082023;
-    String getString(){
-        return "---<значення поля>---" + intVal1;
-    }
+        Integer intVal1 = 24082023;
+        int getValue1(){
+                System.out.println("---<" + intVal1 + ">---");
+                return  intVal1;
+        }
+
+
 }
